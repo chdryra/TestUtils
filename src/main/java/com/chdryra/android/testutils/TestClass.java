281fec1ad848b77702cf82e7f83a62821b1c9530
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2014-12-05 21:41:18 +0000

    Test commit of TestUtils

---
+++ src/main/java/com/chdryra/android/testutils/TestClass.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2014, Rizwan Choudrey - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Author: Rizwan Choudrey
+ * Date: 5 December, 2014
+ */
+
+package com.chdryra.android.testutils;
+
+/**
+ * Created by: Rizwan Choudrey
+ * On: 05/12/2014
+ * Email: rizwan.choudrey@gmail.com
+ */
+public class TestClass {
+}
