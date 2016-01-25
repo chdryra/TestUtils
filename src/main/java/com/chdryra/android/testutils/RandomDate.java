563b45536c773b6070cdb4b90489958592c8b7fc
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2016-01-25 18:22:26 +0000

    Added test for FactoryReviewerDbTableRow

--- src/main/java/com/chdryra/android/testutils/RandomDate.java
+++ src/main/java/com/chdryra/android/testutils/RandomDate.java
@@ -22,7 +22,7 @@ public class RandomDate {
 
     //Static methods
     public static Date nextDate() {
-        int year = 1900 + RAND.nextInt(115);
+        int year = 1976 + RAND.nextInt(40);
         int dayOfYear = 1 + RAND.nextInt(364);
 
         Calendar calendar = Calendar.getInstance();
